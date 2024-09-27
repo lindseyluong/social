dbd296aa9f85b3a4b830255b939179e064bebaa4
lindseyluong <lindseyluong@yahoo.com>
2024-09-27 00:40:30 -0500

    basic posts and profile set up

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+
+export const supabaseUrl = 'https://qknzmxeutwjcorwcryje.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrbnpteGV1dHdqY29yd2NyeWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczOTc1OTgsImV4cCI6MjA0Mjk3MzU5OH0.sbpr0M2F671Y_r-smT_9MirUINB1h2-4ofc8AZMDsds';
