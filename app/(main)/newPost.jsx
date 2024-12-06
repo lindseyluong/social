938d7d86009beabc923b4b54c5dd6ababd152bd3
Priscila Picazo <picazo57@outlook.com>
2024-12-05 21:57:14 -0600

    song/artist selected in newPost is now displayed in the postcard

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,21 +1,21 @@
-import { View, Text, StyleSheet, ScrollView, Pressable, Image as RNImage, Alert, TouchableOpacity, TextInput } from 'react-native'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import ScreenWrapper from '../../components/ScreenWrapper'
-import { hp, wp } from '../../helpers/common'
-import { theme } from '../../constants/theme'
-import { useAuth } from '../../contexts/AuthContext'
-import { getFilePath, getSupabaseFileUrl, getUserImageSrc, uploadFile } from '../../services/imageService'
-import { Image } from 'expo-image'
-import RichTextEditor from '../../components/RichTextEditor'
-import Button from '../../components/Button'
-import { AntDesign, FontAwesome, FontAwesome6, Ionicons } from '@expo/vector-icons'
+import { View, Text, StyleSheet, ScrollView, Pressable, Image as RNImage, Alert, TouchableOpacity, TextInput } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import ScreenWrapper from '../../components/ScreenWrapper';
+import { hp, wp } from '../../helpers/common';
+import { theme } from '../../constants/theme';
+import { useAuth } from '../../contexts/AuthContext';
+import { getFilePath, getSupabaseFileUrl, getUserImageSrc, uploadFile } from '../../services/imageService';
+import { Image } from 'expo-image';
+import RichTextEditor from '../../components/RichTextEditor';
+import Button from '../../components/Button';
+import { AntDesign, FontAwesome, FontAwesome6, Ionicons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
 import { Video, AVPlaybackStatus } from 'expo-av';
-import { createOrUpdatePost } from '../../services/postService'
-import Header from '../../components/Header'
-import { useLocalSearchParams, useRouter } from 'expo-router'
-import Avatar from '../../components/Avatar'
-import Icon from '../../assets/icons'
+import { createOrUpdatePost } from '../../services/postService';
+import Header from '../../components/Header';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import Avatar from '../../components/Avatar';
+import Icon from '../../assets/icons';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -32,7 +32,6 @@ const SpotifySearch = ({ onSelectSong }) => {
       const expirationDate = await AsyncStorage.getItem('expirationDate');
       const currentTime = Date.now();
 
-      // Check if the token is still valid
       if (!token || !expirationDate || currentTime >= parseInt(expirationDate)) {
         console.error('Spotify token is either invalid or expired. Please authenticate again.');
         Alert.alert("Spotify Authentication", "Please re-authenticate with Spotify to continue.");
@@ -85,26 +84,25 @@ const SpotifySearch = ({ onSelectSong }) => {
 };
 
 const NewPost = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
   const post = useLocalSearchParams();
-  console.log('post: ', post);
   const [file, setFile] = useState(null);
   const bodyRef = useRef('');
   const [loading, setLoading] = useState(false);
   const editorRef = useRef(null);
   const router = useRouter();
-  const [selectedSong, setSelectedSong] = useState(null);  // State to store selected Spotify song
+  const [selectedSong, setSelectedSong] = useState(null);
 
-  useEffect(()=>{
-    if(post && post.id){
+  useEffect(() => {
+    if (post && post.id) {
       bodyRef.current = post.body;
       setFile(post.file || null);
-      setSelectedSong(post.song || null);  // Load the existing song if editing
+      setSelectedSong(post.song || null);
       setTimeout(() => {
         editorRef?.current?.setContentHTML(post.body);
-      }, (300));
+      }, 300);
     }
-  },[])
+  }, []);
 
   const onPick = async (isImage) => {
     let mediaConfig = {
@@ -112,13 +110,13 @@ const NewPost = () => {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 0.7,
-    }
+    };
 
-    if(!isImage){
+    if (!isImage) {
       mediaConfig = {
         mediaTypes: ImagePicker.MediaTypeOptions.Videos,
         allowsEditing: true,
-      }
+      };
     }
     let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
 
@@ -127,8 +125,8 @@ const NewPost = () => {
     }
   };
 
-  const onSubmit = async ()=>{
-    if(!bodyRef.current && !file && !selectedSong){
+  const onSubmit = async () => {
+    if (!bodyRef.current && !file && !selectedSong) {
       Alert.alert('Post', "Please choose an image, add post body, or select a song!");
       return;
     }
@@ -138,134 +136,102 @@ const NewPost = () => {
       file,
       body: bodyRef.current,
       userId: user?.id,
-      song: selectedSong,  // Include the selected song in the post data
-    }
-    if(post && post.id) data.id = post.id;
+      song: selectedSong,
+    };
+    if (post && post.id) data.id = post.id;
 
     let res = await createOrUpdatePost(data);
     setLoading(false);
-    if(res.success){
+    if (res.success) {
       setFile(null);
       bodyRef.current = '';
       setSelectedSong(null);
       editorRef.current?.setContentHTML('');
       router.back();
-    }else{
+    } else {
       Alert.alert('Post', res.msg);
     }
-  }
-
-  const isLocalFile = file=>{
-    if(!file) return null;
-    if(typeof file == 'object') return true;
-    return false;
-  }
-
-  const getFileType = file=>{
-    if(!file) return null;
-    if(isLocalFile(file)){
-      return file.type;
-    }
-    if(file.includes('postImages')){
-      return 'image';
-    }
-    return 'video';
-  }
-
-  const getFileUri = file=>{
-    if(!file) return null;
-    if(isLocalFile(file)){
-      return file.uri;
-    }else{
-      return getSupabaseFileUrl(file)?.uri;
-    }
-  }
+  };
 
   return (
     <ScreenWrapper bg="white">
       <View style={styles.container}>
         <Header title="Create Post" mb={15} />
-          
-        <ScrollView contentContainerStyle={{gap: 20}}>
+        <ScrollView contentContainerStyle={{ gap: 20 }}>
           <View style={styles.header}>
-              <Avatar
-                uri={user?.image}
-                size={hp(6.5)}
-                rounded={theme.radius.xl}
-              />
-              <View style={{gap: 2}}>
-                <Text style={styles.username}>{user && user.name}</Text>
-                <Text style={styles.publicText}>Public</Text>
-              </View>
+            <Avatar
+              uri={user?.image}
+              size={hp(6.5)}
+              rounded={theme.radius.xl}
+            />
+            <View style={{ gap: 2 }}>
+              <Text style={styles.username}>{user && user.name}</Text>
+              <Text style={styles.publicText}>Public</Text>
+            </View>
           </View>
           <View style={styles.textEditor}>
-            <RichTextEditor editorRef={editorRef} onChange={body=> bodyRef.current = body} />
+            <RichTextEditor editorRef={editorRef} onChange={body => bodyRef.current = body} />
           </View>
-          {
-            file && (
-              <View style={styles.file}>
-                {
-                  getFileType(file)=='video'? (
-                    <Video
-                      style={{flex: 1}}
-                      source={{
-                        uri: getFileUri(file)
-                      }}
-                      useNativeControls
-                      resizeMode="cover"
-                      isLooping
-                    />
-                  ):(
-                    <Image source={{uri: getFileUri(file)}} contentFit='cover' style={{flex: 1}} />
-                  )
-                }
-                <Pressable style={styles.closeIcon} onPress={()=> setFile(null)}>
-                  <AntDesign name="closecircle" size={25} color="rgba(255, 0,0,0.6)" />
-                </Pressable>
-              </View>
-            )
-          }  
-          {
-            selectedSong && (
-              <View style={styles.songContainer}>
-                <Text style={styles.songTitle}>{selectedSong.name} by {selectedSong.artist}</Text>
-                <Pressable style={styles.closeIcon} onPress={() => setSelectedSong(null)}>
-                  <AntDesign name="closecircle" size={25} color="rgba(255, 0,0,0.6)" />
-                </Pressable>
-              </View>
-            )
-          } 
+          {file && (
+            <View style={styles.file}>
+              {getFileType(file) == 'video' ? (
+                <Video
+                  style={{ flex: 1 }}
+                  source={{
+                    uri: getFileUri(file)
+                  }}
+                  useNativeControls
+                  resizeMode="cover"
+                  isLooping
+                />
+              ) : (
+                <Image source={{ uri: getFileUri(file) }} contentFit='cover' style={{ flex: 1 }} />
+              )}
+              <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+                <AntDesign name="closecircle" size={25} color="rgba(255, 0,0,0.6)" />
+              </Pressable>
+            </View>
+          )}
+          {selectedSong && (
+            <View style={styles.songContainer}>
+              <Text style={styles.songTitle}>{selectedSong.name} by {selectedSong.artist}</Text>
+              <Pressable style={styles.closeIcon} onPress={() => setSelectedSong(null)}>
+                <AntDesign name="closecircle" size={25} color="rgba(255, 0,0,0.6)" />
+              </Pressable>
+            </View>
+          )}
           <View style={styles.media}>
             <Text style={styles.addImageText}>Add to your post</Text>
             <View style={styles.mediaIcons}>
-              <TouchableOpacity onPress={()=> onPick(true)}>
+              <TouchableOpacity onPress={() => onPick(true)}>
                 <Icon name="image" size={30} color={theme.colors.dark} />
               </TouchableOpacity>
-              <TouchableOpacity onPress={()=> onPick(false)}>
+              <TouchableOpacity onPress={() => onPick(false)}>
                 <Icon name="video" size={33} color={theme.colors.dark} />
               </TouchableOpacity>
               <SpotifySearch onSelectSong={(song) => setSelectedSong(song)} />
             </View>
-          </View> 
+          </View>
         </ScrollView>
-        <Button 
-          buttonStyle={{height: hp(6.2)}} 
-          title={post && post.id? "Update": "Post"}
-          loading={loading}
-          hasShadow={false} 
-          onPress={onSubmit}
-        />
+        <View style={styles.footerButtonContainer}>
+          <Button
+            buttonStyle={{ height: hp(6.2) }}
+            title={post && post.id ? "Update" : "Post"}
+            loading={loading}
+            hasShadow={false}
+            onPress={onSubmit}
+          />
+        </View>
       </View>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginBottom: 30,
     paddingHorizontal: wp(4),
-    gap: 15,
+    paddingBottom: hp(2),
   },
   header: {
     flexDirection: 'row',
@@ -282,43 +248,20 @@ const styles = StyleSheet.create({
     fontWeight: theme.fonts.medium,
     color: theme.colors.textLight,
   },
-  textEditor: {},
-  media: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderWidth: 1.5,
-    padding: 12,
-    paddingHorizontal: 18,
-    borderRadius: theme.radius.xl,
-    borderCurve: 'continuous',
-    borderColor: theme.colors.gray
-  },
-  mediaIcons: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 15
-  },
-  addImageText: {
-    fontSize: hp(1.9),
-    fontWeight: theme.fonts.semibold,
-    color: theme.colors.text,
+  textEditor: {
+    marginVertical: 10,
   },
   file: {
     height: hp(30),
     width: '100%',
     borderRadius: theme.radius.xl,
     overflow: 'hidden',
-    borderCurve: 'continuous'
   },
   songContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
     padding: 10,
-    borderWidth: 1,
-    borderColor: theme.colors.gray,
+    backgroundColor: theme.colors.lightGray,
     borderRadius: theme.radius.md,
+    marginBottom: 10,
   },
   songTitle: {
     fontSize: hp(2),
@@ -329,20 +272,30 @@ const styles = StyleSheet.create({
     top: 10,
     right: 10,
   },
-  spotifySearchContainer: {
-    marginTop: 10,
-  },
-  searchInput: {
-    borderWidth: 1,
+  media: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1.5,
+    padding: 12,
+    paddingHorizontal: 18,
+    borderRadius: theme.radius.xl,
     borderColor: theme.colors.gray,
-    padding: 10,
-    borderRadius: theme.radius.md,
   },
-  trackText: {
-    paddingVertical: 5,
-    fontSize: hp(1.8),
+  mediaIcons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+  },
+  addImageText: {
+    fontSize: hp(1.9),
+    fontWeight: theme.fonts.semibold,
     color: theme.colors.text,
-  }
-})
+  },
+  footerButtonContainer: {
+    paddingHorizontal: wp(4),
+    marginBottom: hp(2),
+  },
+});
 
 export default NewPost;
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { theme } from '../constants/theme';
 import { Image } from 'expo-image';
 import { downloadFile, getSupabaseFileUrl, getUserImageSrc } from '../services/imageService';
@@ -15,18 +15,18 @@ import Avatar from './Avatar';
 
 const textStyle = {
   color: theme.colors.dark,
-  fontSize: hp(1.75)
-}
+  fontSize: hp(1.75),
+};
 
 const tagsStyles = {
   div: textStyle,
   p: textStyle,
   ol: textStyle,
   h1: {
-    color: theme.colors.dark
+    color: theme.colors.dark,
   },
   h4: {
-    color: theme.colors.dark
+    color: theme.colors.dark,
   },
 };
 
@@ -34,16 +34,17 @@ const PostCard = ({
   item,
   currentUser,
   router,
-  showMoreIcon=true,
-  hasShadow=true,
-  showDelete=false,
-  onDelete=()=>{},
-  onEdit=()=>{}
+  showMoreIcon = true,
+  hasShadow = true,
+  showDelete = false,
+  onDelete = () => {},
+  onEdit = () => {},
 }) => {
   const [likes, setLikes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [parsedSong, setParsedSong] = useState(null);
 
-  const liked = likes.filter(like=> like.userId==currentUser?.id)[0]? true: false;
+  const liked = likes.filter((like) => like.userId == currentUser?.id)[0] ? true : false;
   const createdAt = moment(item?.created_at).format('MMM D');
   const htmlBody = { html: item?.body };
   const shadowStyles = {
@@ -54,215 +55,197 @@ const PostCard = ({
     shadowOpacity: 0.06,
     shadowRadius: 6,
     elevation: 1,
-  }
-  // console.log('item: ', item.comments);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     setLikes(item?.postLikes);
-  },[]);
 
+    // Parse the song JSON string if it exists
+    if (item?.song) {
+      try {
+        const songData = JSON.parse(item.song);
+        setParsedSong(songData);
+      } catch (error) {
+        console.error('Error parsing song data:', error);
+      }
+    }
+  }, [item]);
 
-  
-  const onLike = async ()=>{
-
-    if(liked){
-      let updatedLikes = likes.filter(like=> like.userId!=currentUser?.id);
+  const onLike = async () => {
+    if (liked) {
+      let updatedLikes = likes.filter((like) => like.userId != currentUser?.id);
       setLikes([...updatedLikes]);
 
       let res = await removePostLike(item?.id, currentUser?.id);
-      console.log('res: ', res);
-      if(!res.success){
-        Alert.alert('Post', 'Something went wrong!')
+      if (!res.success) {
+        Alert.alert('Post', 'Something went wrong!');
       }
-    }else{
+    } else {
       let data = {
         userId: currentUser?.id,
         postId: item?.id,
-      }
+      };
 
       setLikes([...likes, data]);
       let res = await createPostLike(data);
-      console.log('res: ', res);
-      if(!res.success){
-        Alert.alert('Post', 'Something went wrong!')
+      if (!res.success) {
+        Alert.alert('Post', 'Something went wrong!');
       }
     }
-    
-  }
+  };
 
-  const onShare = async ()=>{
-    let content = {message: stripHtmlTags(item?.body)};
-    if(item?.file){
+  const onShare = async () => {
+    let content = { message: stripHtmlTags(item?.body) };
+    if (item?.file) {
       setLoading(true);
       let uri = await downloadFile(getSupabaseFileUrl(item.file).uri);
       content.url = uri;
       setLoading(false);
     }
     Share.share(content);
-      
-  }
+  };
 
-  const handlePostDelete = ()=>{
+  const handlePostDelete = () => {
     Alert.alert('Confirm', 'Are you sure you want to do this?', [
-        {
-          text: 'Cancel',
-          onPress: () => console.log('Cancel delete'),
-          style: 'cancel',
-        },
-        {
-            text: 'Delete', 
-            onPress: () => onDelete(item),
-            style: 'destructive'
-        },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel delete'),
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        onPress: () => onDelete(item),
+        style: 'destructive',
+      },
     ]);
-  }
+  };
 
-  const openPostDetails = ()=>{
-    router.push({pathname: 'postDetails', params: {postId: item?.id}})
-  }
+  const openPostDetails = () => {
+    router.push({ pathname: 'postDetails', params: { postId: item?.id } });
+  };
+
   return (
     <View style={[styles.container, hasShadow && shadowStyles]}>
       <View style={styles.header}>
         {/* user info and post time */}
         <View style={styles.userInfo}>
-          <Avatar 
-            size={hp(4.5)}
-            uri={item?.user?.image}
-            rounded={theme.radius.md}
-          />
-          <View style={{gap: 2}}>
+          <Avatar size={hp(4.5)} uri={item?.user?.image} rounded={theme.radius.md} />
+          <View style={{ gap: 2 }}>
             <Text style={styles.username}>{item?.user?.name}</Text>
             <Text style={styles.postTime}>{createdAt}</Text>
           </View>
         </View>
 
         {/* actions */}
-        {
-          showMoreIcon && (
-            <TouchableOpacity onPress={openPostDetails}>
-              <Icon name="threeDotsHorizontal" size={hp(3.4)} strokeWidth={3} color={theme.colors.text} />
+        {showMoreIcon && (
+          <TouchableOpacity onPress={openPostDetails}>
+            <Icon name="threeDotsHorizontal" size={hp(3.4)} strokeWidth={3} color={theme.colors.text} />
+          </TouchableOpacity>
+        )}
+        {showDelete && currentUser.id == item.userId && (
+          <View style={styles.actions}>
+            <TouchableOpacity onPress={() => onEdit(item)}>
+              <Icon name="edit" size={hp(2.5)} color={theme.colors.text} />
             </TouchableOpacity>
-          )
-        }
-        {
-          showDelete && currentUser.id==item.userId && (
-            <View style={styles.actions}>
-              <TouchableOpacity onPress={()=> onEdit(item)}>
-                <Icon name="edit" size={hp(2.5)} color={theme.colors.text} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={handlePostDelete}>
-                <Icon name="delete" size={hp(2.5)} color={theme.colors.rose} />
-              </TouchableOpacity>
-            </View>
-          )
-        }
+            <TouchableOpacity onPress={handlePostDelete}>
+              <Icon name="delete" size={hp(2.5)} color={theme.colors.rose} />
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
 
       {/* post image & body */}
       <View style={styles.content}>
         <View style={styles.postBody}>
-          {
-            item?.body && (
-              <RenderHtml
-                contentWidth={wp(100)}
-                source={htmlBody}
-                tagsStyles={tagsStyles}
-                render
-              />
-            )
-          }
+          {item?.body && (
+            <RenderHtml contentWidth={wp(100)} source={htmlBody} tagsStyles={tagsStyles} />
+          )}
         </View>
-        
+
+        {/* Display the song information if it exists */}
+        {parsedSong && parsedSong.name && parsedSong.artist && (
+          <View style={styles.songContainer}>
+            <Text style={styles.songTitle}>🎵 {parsedSong.name} by {parsedSong.artist}</Text>
+          </View>
+        )}
+
         {/* post image */}
-        {
-          item?.file && item?.file?.includes('postImages') && (
-            <Image 
-              source={getSupabaseFileUrl(item?.file)}
-              transition={100}
-              style={styles.postMedia}
-              contentFit='cover'
-            />
-          )
-        }
+        {item?.file && item?.file?.includes('postImages') && (
+          <Image
+            source={getSupabaseFileUrl(item?.file)}
+            transition={100}
+            style={styles.postMedia}
+            contentFit="cover"
+          />
+        )}
 
         {/* post video */}
-        {
-          item?.file && item?.file?.includes('postVideos') && (
-            <Video
-              style={[styles.postMedia, {height: hp(30)}]}
-              source={getSupabaseFileUrl(item?.file)}
-              useNativeControls
-              resizeMode="cover"
-              isLooping
-            />
-          )
-        }
+        {item?.file && item?.file?.includes('postVideos') && (
+          <Video
+            style={[styles.postMedia, { height: hp(30) }]}
+            source={getSupabaseFileUrl(item?.file)}
+            useNativeControls
+            resizeMode="cover"
+            isLooping
+          />
+        )}
       </View>
 
       {/* like & comment */}
       <View style={styles.footer}>
         <View style={styles.footerButton}>
           <TouchableOpacity onPress={onLike}>
-            <Icon name="heart" fill={liked? theme.colors.rose: 'transparent'} size={24} color={liked? theme.colors.rose: theme.colors.textLight} />
+            <Icon
+              name="heart"
+              fill={liked ? theme.colors.rose : 'transparent'}
+              size={24}
+              color={liked ? theme.colors.rose : theme.colors.textLight}
+            />
           </TouchableOpacity>
-          <Text style={styles.count}>
-            {
-              likes?.length
-            }
-          </Text>
+          <Text style={styles.count}>{likes?.length}</Text>
         </View>
         <View style={styles.footerButton}>
           <TouchableOpacity onPress={openPostDetails}>
             <Icon name="comment" size={24} color={theme.colors.textLight} />
           </TouchableOpacity>
-          <Text style={styles.count}>
-            {/* implement after adding post details modal */}
-            {/* 0 */}
-            {
-              item?.comments[0]?.count
-            }
-
-          </Text>
+          <Text style={styles.count}>{item?.comments[0]?.count}</Text>
         </View>
         <View style={styles.footerButton}>
-          {
-            loading? (
-              <Loading size="small" />
-            ):(
-              <TouchableOpacity onPress={onShare}>
-                <Icon name="share" size={24} color={theme.colors.textLight} />
-              </TouchableOpacity>
-            )
-          }
-          
+          {loading ? (
+            <Loading size="small" />
+          ) : (
+            <TouchableOpacity onPress={onShare}>
+              <Icon name="share" size={24} color={theme.colors.textLight} />
+            </TouchableOpacity>
+          )}
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     gap: 10,
     marginBottom: 15,
-    borderRadius: theme.radius.xxl*1.1,
+    borderRadius: theme.radius.xxl * 1.1,
     borderCurve: 'continuous',
     padding: 10,
     paddingVertical: 12,
     backgroundColor: 'white',
     borderWidth: 0.5,
     borderColor: theme.colors.gray,
-    shadowColor: '#000'
+    shadowColor: '#000',
   },
   header: {
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   userInfo: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8
-  },  
+    gap: 8,
+  },
   username: {
     fontSize: hp(1.7),
     color: theme.colors.textDark,
@@ -275,38 +258,46 @@ const styles = StyleSheet.create({
   },
   content: {
     gap: 10,
-    // marginBottom: 10,
   },
   postMedia: {
     height: hp(40),
     width: '100%',
     borderRadius: theme.radius.xl,
-    borderCurve: 'continuous'
+    borderCurve: 'continuous',
   },
   postBody: {
     marginLeft: 5,
   },
   footer: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     alignItems: 'center',
-    gap: 15
+    gap: 15,
   },
   footerButton: {
     marginLeft: 5,
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 4
+    gap: 4,
   },
   actions: {
     flexDirection: 'row',
-    alignItems: 'center', 
+    alignItems: 'center',
     gap: 18,
   },
   count: {
     color: theme.colors.text,
-    fontSize: hp(1.8)
-  }
+    fontSize: hp(1.8),
+  },
+  songContainer: {
+    padding: 10,
+    backgroundColor: theme.colors.lightGray,
+    borderRadius: theme.radius.md,
+    marginBottom: 10,
+  },
+  songTitle: {
+    fontSize: hp(2),
+    color: theme.colors.text,
+  },
+});
 
-})
-
-export default PostCard
+export default PostCard;
